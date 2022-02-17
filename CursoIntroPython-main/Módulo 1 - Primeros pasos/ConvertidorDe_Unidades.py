fc019c2d5e550e8a995340283597e222de5137fa
HiYeaismin8a <48936318+HiYeaismin8a@users.noreply.github.com>
2022-02-17 14:21:34 -0700

    Delete CursoIntroPython-main directory

--- CursoIntroPython-main/Módulo 1 - Primeros pasos/ConvertidorDe_Unidades.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-parsec = input("Ingresa la cantidad de Parsec: ")
-lightyears = 3.26156 * int(parsec)               # 1 parsec es 3.26156 años luz. 
-
-
-print("In "+ str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
-print("Today's date is: " + str(date.today()))
