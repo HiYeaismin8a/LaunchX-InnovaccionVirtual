6332ecb536c1a3cae81e8a1685d4fdb31e0ace60
HiYeaismin8a <48936318+HiYeaismin8a@users.noreply.github.com>
2022-02-17 14:04:02 -0700

    Add files via upload

---
+++ CursoIntroPython-main/Módulo 5 - Usar operaciones matemáticas/input.py
@@ -0,0 +1,10 @@
+planeta1 = input("Ingresa el valor del primer planeta: ")
+planeta2 = input("Ingresa el valor del 2do planeta: ")
+planeta1_1 = int(planeta1)
+planeta2_2 = int(planeta2)
+
+print(planeta1_1)
+print(planeta2_2)
+
+#absoluto y al primer planeta restarle el segundo.
+print( abs("Diferencia entre planetas: " +planeta1_1 -  planeta2_2))
